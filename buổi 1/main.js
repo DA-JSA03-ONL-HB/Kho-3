2ebcdbaca72681d386d1f433371aca055291e6e1
DA-JSA03-ONL-HB <160859404+DA-JSA03-ONL-HB@users.noreply.github.com>
2024-07-04 19:10:04 +0700

    g

---
+++ buổi 1/main.js
@@ -0,0 +1,65 @@
+//cửa sổ console log
+// console.log('hello')
+// alert('MINDX')
+// prompt('Bạn tên gì')//cho nhập ô dữ liệu
+// //Các dữ liệu kiểu số
+// //const:Dùng cho hằng số
+// const myName = 'Khoi'
+//var: khai báo cho biến toàn cục(trong mọi phạm vi)
+//let ; khai báo cho biến cục bộ(trong phạm vi nào đó)
+//scope; phạm vi
+// {
+//     var myAge = 20;
+// }
+// console.log(myAge)
+
+
+// let number1 = 1;
+// let number2 = 2;
+// let sumNumber = number1+number2;
+// console.log(sumNumber);
+//Kiểu chuỗi(bắt buộc phải có '')
+// let chuoi1 = 'Hello';
+// let chuoi2 = 'My name is khoi';
+// let noichuoi = chuoi1+''+chuoi2
+// console.log(noichuoi);
+// kiểu undefiinded: ko xác định 
+// let a;
+// console.log(a);
+//kiểu null: giá trị rỗng ko tồn tại
+// cách kiểm tra dữ liệu
+// let number1 = 1;// number
+// let number2 = '2';//string 
+// // console.log(typeof(number1));
+// //Toán tử so sánh
+// if (number1===number2){
+//     console.log('Bằng nhau')
+// }//3 dấu= là ko bằng, 2 dấu = là bằng nhau
+// else{
+//     console.log('ko bằng')
+// }
+//câu lệnh if else
+
+// if (myName == 'khoi') {
+//     console.log('xin chao'+ myName)
+// }
+// else if (myName== 'Khoi'){
+//     alert ('hello khoi')
+// }
+// else{
+//     alert('hello')
+// }
+
+let diemthi = prompt ('Số điểm')
+if (diemthi <=4 ){
+    alert ('Trung Bình');
+}
+else if (diemthi <=7 ) {
+    alert('Khá');
+}
+else if (diemthi <=10){
+    alert ('Giỏi')
+}
+else if (diemthi =10) {
+    alert ('Xuất sắc')
+}
