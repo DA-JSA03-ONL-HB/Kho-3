305e543a7c8ea0cb01ee535b2d6764f1da47f0c5
DA-JSA03-ONL-HB <160859404+DA-JSA03-ONL-HB@users.noreply.github.com>
2024-09-14 08:25:01 -0700

    g
    g

--- sản phẩm cuối khóa/login.js
+++ sản phẩm cuối khóa/login.js
@@ -1,39 +1,41 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "http://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+//  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCnXIpv7h7LvbGB7JjDBm3lRVaBNWefZro",
-  authDomain: "spck-3ef92.firebaseapp.com",
-  projectId: "spck-3ef92",
-  storageBucket: "spck-3ef92.appspot.com",
-  messagingSenderId: "168618497545",
-  appId: "1:168618497545:web:e1a83c92defec4ee478ad8"
+    apiKey: "AIzaSyCnXIpv7h7LvbGB7JjDBm3lRVaBNWefZro",
+    authDomain: "spck-3ef92.firebaseapp.com",
+    projectId: "spck-3ef92",
+    storageBucket: "spck-3ef92.appspot.com",
+    messagingSenderId: "168618497545",
+    appId: "1:168618497545:web:e1a83c92defec4ee478ad8"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-//submit button
+//  const userName = document.getElementById('userName').value;
 
+//submit
 const submit = document.getElementById('submit');
-submit.addEventListener('click', (event) => {
-    event.preventDefault();
+submit.addEventListener('click', function (event) {
+    event.preventDefault()
 
-    //inputs
-    const email = document.getElementById('email').value;
+    //input
     const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
 
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed up 
             const user = userCredential.user;
-            alert('Đăng ký...')
-            window.location.href = './index1.html'
+            alert('Creating account ...')
             // ...
         })
         .catch((error) => {
@@ -42,7 +44,7 @@ submit.addEventListener('click', (event) => {
             alert(errorMessage)
             // ..
         });
-});
+})
 // function toggleForm() {
 //     const formContainer = document.getElementById('form-container');
 //     const formTitle = document.getElementById('form-title');
@@ -101,7 +103,7 @@ submit.addEventListener('click', (event) => {
 
 //     if (storedUser && storedUser.username === username && storedUser.password === password) {
 //         alert(`Login successful: ${username}`);
-//         window.location.href = 'homepage.html'; 
+//         window.location.href = 'homepage.html';
 //     } else {
 //         alert("Tên hoặc pass ko đúng.");
 //     }
