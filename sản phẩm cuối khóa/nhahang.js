b1017f8f4a0e9111af7ad081dd626d3f4f0479be
DA-JSA03-ONL-HB <160859404+DA-JSA03-ONL-HB@users.noreply.github.com>
2024-09-24 09:28:29 -0700

    gg
    gg

---
+++ sản phẩm cuối khóa/nhahang.js
@@ -0,0 +1,16 @@
+
+function dochange(){
+    alert('Đây là trang chủ của Roma Chef');
+  }
+  
+  function doconfirm(){
+     var r=confirm("Vui lòng chọn một nút bấm.");
+    if (r==true){
+      alert('Bạn chọn OK.');
+    }
+    else{
+      alert('Bạn chọn Cancel.');
+    }
+  }
+  
+  
